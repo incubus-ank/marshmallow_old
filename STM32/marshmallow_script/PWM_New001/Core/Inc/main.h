38d49a659ce8a3b2305f2bcbe0b18a84b037e163
Alexander Kraynikov <kpahyx@gmail.com>
2022-05-31 15:02:46 +0300

    add STM32 code

---
+++ STM32/marshmallow_script/PWM_New001/Core/Inc/main.h
@@ -0,0 +1,90 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_Pin GPIO_PIN_13
+#define LED_GPIO_Port GPIOC
+#define BUTTON_Pin GPIO_PIN_0
+#define BUTTON_GPIO_Port GPIOC
+#define L_AIN2_Pin GPIO_PIN_4
+#define L_AIN2_GPIO_Port GPIOC
+#define L_AIN1_Pin GPIO_PIN_5
+#define L_AIN1_GPIO_Port GPIOC
+#define L_BIN1_Pin GPIO_PIN_0
+#define L_BIN1_GPIO_Port GPIOB
+#define L_BIN2_Pin GPIO_PIN_1
+#define L_BIN2_GPIO_Port GPIOB
+#define R_AIN2_Pin GPIO_PIN_12
+#define R_AIN2_GPIO_Port GPIOC
+#define R_AIN1_Pin GPIO_PIN_2
+#define R_AIN1_GPIO_Port GPIOD
+#define R_BIN1_Pin GPIO_PIN_4
+#define R_BIN1_GPIO_Port GPIOB
+#define R_BIN2_Pin GPIO_PIN_5
+#define R_BIN2_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
